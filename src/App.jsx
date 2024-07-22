0ce97ce72e4470f751b8a637bca66f0fbd3551e8
junglesub <junglesubmarine@gmail.com>
2024-07-22 17:41:14 +0000

    feat: add style-components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import styled from "styled-components";
+
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <div className="App">
+      <TestStyle>Hello</TestStyle> World
+    </div>
+  );
 }
+
+const TestStyle = styled.div`
+  color: red;
+`;
 
 export default App;
