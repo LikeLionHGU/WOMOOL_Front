e96853e002c1029eaed0f6e9201a8fb823b7ace1
junglesub <junglesubmarine@gmail.com>
2024-08-04 20:46:30 +0900

    feat(groupviewexplore): add Group view explore display page
    ref #8

---
+++ src/components/Mypage/GroupViewExploreCard.jsx
@@ -0,0 +1,122 @@
+import React from "react";
+import styled from "styled-components";
+import { pretendard } from "../../styles/fonts";
+
+import PeopleIcon from "src/assets/Mypage-group/peopleicon.svg";
+import { convertMlToL } from "src/tools/tool";
+
+function GroupViewExploreCard({ hoverGroupCode = false, data = {} }) {
+  return (
+    <Card
+      style={{
+        backgroundImage: `url(${data.teamImage})`,
+      }}
+    >
+      <Title>{data.name}</Title>
+      <Footer.wrapper>
+        <Footer.left>
+          {convertMlToL(data.groupTotal)}L/{convertMlToL(data.recommendation)}L
+        </Footer.left>
+        <Footer.right>
+          <img src={PeopleIcon} draggable={false} />
+          <div>{data.peopleCount}</div>
+        </Footer.right>
+      </Footer.wrapper>
+      {hoverGroupCode && (
+        <Hover>
+          <div>CODE | {data.code}</div>
+        </Hover>
+      )}
+    </Card>
+  );
+}
+
+export default GroupViewExploreCard;
+
+const Hover = styled.div`
+  display: none;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  background-color: rgba(0, 0, 0, 0.7);
+
+  justify-content: center;
+  align-items: center;
+
+  div {
+    /* CODE | 7942 */
+
+    ${pretendard}
+    font-style: normal;
+    font-weight: 700;
+    font-size: 36px;
+    line-height: 43px;
+    /* identical to box height */
+    text-transform: uppercase;
+
+    color: #ffffff;
+  }
+`;
+
+const Card = styled.div`
+  position: relative;
+  box-sizing: border-box;
+  /* 이 구역의 하마는 나야 */
+  width: 288px;
+  height: 183px;
+  background-color: #d9d9d9;
+  padding: 18px;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  background-size: cover;
+  background-position: center center;
+  background-repeat: no-repeat;
+
+  cursor: pointer;
+  &:hover ${Hover} {
+    display: flex;
+  }
+`;
+
+const Title = styled.div`
+  ${pretendard}
+  font-style: normal;
+  font-weight: 600;
+  font-size: 25px;
+  line-height: 30px;
+  /* identical to box height */
+  text-transform: uppercase;
+
+  color: #000000;
+`;
+
+const Footer = {
+  wrapper: styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-end;
+  `,
+  left: styled.div`
+    /* 7L/10L */
+
+    ${pretendard}
+    font-style: normal;
+    font-weight: 600;
+    font-size: 36px;
+    line-height: 43px;
+    /* identical to box height */
+    text-transform: uppercase;
+
+    color: #000000;
+  `,
+  right: styled.div`
+    display: flex;
+    align-items: center;
+    gap: 5px;
+  `,
+};
--- src/components/Mypage/GroupViewExploreMain.jsx
+++ src/components/Mypage/GroupViewExploreMain.jsx
@@ -1,7 +1,126 @@
-import React from "react";
+import React, { useEffect, useState, useMemo } from "react";
+import styled from "styled-components";
+import { pretendard, timesNewRoman } from "src/styles/fonts";
+
+import { useFetchBe } from "src/tools/api";
+
+import GroupViewExploreCard from "./GroupViewExploreCard";
+import GroupRightArrow from "src/assets/Mypage-group/group-rightarrow.svg";
+import CreateGroup from "src/assets/Mypage-group/createGroup.svg";
+import CreateGroupHover from "src/assets/Mypage-group/createGroup-hover.svg";
+import { HoverImageSpan } from "../../styles/stylePresets";
 
 function GroupViewExploreMain() {
-  return <div>GroupViewExploreMain</div>;
+  const fetchBe = useFetchBe();
+  const [allGroups, setAllGroups] = useState();
+  const [myGroups, setMyGroups] = useState();
+  const allGroupByCode = useMemo(() => {
+    return allGroups?.reduce((prev, curr) => {
+      prev[curr.code] = curr;
+      return prev;
+    }, {});
+  }, [allGroups]);
+
+  useEffect(() => {
+    fetchBe("/team/allGroups").then((json) => setAllGroups(json.teams));
+    fetchBe("/user/getGroups").then((json) => setMyGroups(json.teams));
+  }, []);
+
+  if (!allGroups || !myGroups) return <div></div>;
+  return (
+    <Wrapper>
+      <Section>
+        <SectionHeader>
+          <div className="top">
+            <div>Explore</div>
+            <img src={GroupRightArrow} draggable={false} />
+          </div>
+          <div className="bottom">
+            현재까지 {allGroups.length}개의 그룹이 우물하고 있어요
+          </div>
+        </SectionHeader>
+        <SectionBody>
+          {allGroups.map((group) => (
+            <GroupViewExploreCard
+              key={group.code}
+              data={group}
+              hoverGroupCode={true}
+            />
+          ))}
+        </SectionBody>
+      </Section>
+      <Section>
+        <SectionHeader>
+          <div className="top">
+            <div>My Group</div>
+            <img src={GroupRightArrow} draggable={false} />
+          </div>
+          <div className="bottom">
+            총 {myGroups.length}개의 그룹에 가입되어 있어요
+          </div>
+        </SectionHeader>
+        <SectionBody>
+          {myGroups.map(({ code }) => (
+            <GroupViewExploreCard
+              key={code}
+              data={allGroupByCode[code]}
+              hoverGroupCode={true}
+            />
+          ))}
+          <HoverImageSpan>
+            <img src={CreateGroup} draggable={false} />
+            <img className="hover" src={CreateGroupHover} draggable={false} />
+          </HoverImageSpan>
+        </SectionBody>
+      </Section>
+    </Wrapper>
+  );
 }
 
 export default GroupViewExploreMain;
+
+const Wrapper = styled.div`
+  padding-top: 42px;
+`;
+
+const Section = styled.div`
+  margin-bottom: 45px;
+`;
+
+const SectionHeader = styled.div`
+  margin-left: 58px;
+  .top {
+    display: flex;
+    margin-bottom: 5px;
+    div {
+      margin-right: 20px;
+      ${pretendard}
+      font-family: "Pretendard";
+      font-style: normal;
+      font-weight: 700;
+      font-size: 28px;
+      line-height: 33px;
+      text-transform: uppercase;
+
+      color: #000000;
+    }
+  }
+  .bottom {
+    margin-bottom: 5px;
+    ${pretendard}
+  }
+`;
+
+const SectionBody = styled.div`
+  box-sizing: border-box;
+  width: 100%;
+  overflow-x: auto;
+  padding: 16px 58px;
+  padding-right: 16px;
+  display: flex;
+  gap: 24px;
+
+  & > div {
+    flex-shrink: 0;
+  }
+`;
